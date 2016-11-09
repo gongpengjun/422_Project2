b81b08c5a1aeb48d21e9d6622510930eb69498ca
root <root@ip-172-31-28-37.us-west-2.compute.internal>
2016-11-09 04:46:14 +0000

    edited signIn.php page

--- views/logged_in.php
+++ views/logged_in.php
@@ -1,6 +1,5 @@
 <!-- if you need user information, just put them into the $_SESSION variable and output them here -->
 Hey, <?php echo $_SESSION['user_name']; ?>. You are logged in.
-Try to close this browser tab and open it again. Still logged in! ;)
+If you close your browser and come back to this site, then you should still be logged in :)
 
-<!-- because people were asking: "index.php?logout" is just my simplified form of "index.php?logout=true" -->
 <a href="signIn.php?logout">Logout</a>
