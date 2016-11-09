599894cd4e820e326849ab0483ef431efda71d8a
root <root@ip-172-31-28-37.us-west-2.compute.internal>
2016-11-09 04:54:49 +0000

    Added (or Email) to login page

--- views/not_logged_in.php
+++ views/not_logged_in.php
@@ -17,7 +17,7 @@ if (isset($login)) {
 <!-- login form box -->
 <form method="post" action="signIn.php" name="loginform">
 
-    <label for="login_input_username">Username</label>
+    <label for="login_input_username">Username (or Email)</label>
     <input id="login_input_username" class="login_input" type="text" name="user_name" required />
 
     <label for="login_input_password">Password</label>
