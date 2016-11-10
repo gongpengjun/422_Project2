e4bf615b379317600a4731430572ad7d2c3afcb5
root <root@ip-172-31-28-37.us-west-2.compute.internal>
2016-11-10 19:56:20 +0000

    added php footer template

---
+++ memorycard.php
@@ -0,0 +1,58 @@
+<html>
+<head>
+
+<title>Dolla Tree Memorycard</title>
+<link href="dolla.css" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body>
+
+
+<div id="header">
+	Free Dolla Games
+</div>
+
+<div id="intro">
+	Welcome to memorycard. This is how you play...<br><br>
+</div>
+
+<div id="pageStuff">
+	THIS IS WHERE MEMORYCARD WILL GO<br>
+	Will Stidhem is the best<br>
+	THIS IS WHERE MEMORYCARD WILL GO<br>
+	Will Stidhem is the best<br>
+	THIS IS WHERE MEMORYCARD WILL GO<br>
+	Will Stidhem is the best<br>
+	THIS IS WHERE MEMORYCARD WILL GO<br>
+	Will Stidhem is the best<br>
+	THIS IS WHERE MEMORYCARD WILL GO<br>
+	Will Stidhem is the best<br>
+	THIS IS WHERE MEMORYCARD WILL GO<br>
+	Will Stidhem is the best<br>
+	THIS IS WHERE MEMORYCARD WILL GO<br>
+	Will Stidhem is the best<br>
+
+
+</div>
+
+<table border="1" align="center">
+	<tr>
+		<td id ="" height = "20px" width = "150px" name="">Your High Score</td>
+		<td id ="" height = "20px" width = "150px" name="">Overall High Score</td>
+	</tr>
+
+	<tr>
+		<td id ="" height = "20px" width = "150px" name=""></td>
+		<td id ="" height = "20px" width = "150px" name=""></td>
+	</tr>
+</table>
+<br><br>
+
+<?php
+	include_once('footer.php');
+?>
+
+
+</body>
+</html>
