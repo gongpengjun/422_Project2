92f922ff9f72696baa99df0930f75d330797f6f5
root <root@ip-172-31-28-37.us-west-2.compute.internal>
2016-11-10 22:35:48 +0000

    fixed accountInfo.php

--- accountInfo.php
+++ accountInfo.php
@@ -1,8 +1,21 @@
 <div id ="intro">
-<?php echo "Welcome ";
-if(isset($_SESSION['user_name'])){
+<?php
+require_once("config/db.php");
+
+// load the login class
+require_once("classes/Login.php");
+
+// create a login object. when this object is created, it will do all login/logout stuff automatically
+// so this single line handles the entire login process. in consequence, you can simply ...
+$login = new Login();
+
+echo "Welcome";
+
+// ... ask if we are logged in here:
+if ($login->isUserLoggedIn() == true) { 
 	echo $_SESSION['user_name'];
-}else{
+} else {
 	echo "Guest";
+}
 ?>
 </div>
