9cbffc9d61a21bbd1fcccaa7b6459fad00385bfe
root <root@ip-172-31-28-37.us-west-2.compute.internal>
2016-11-10 20:47:32 +0000

    Converted all html files to php and added the footer template to each page except the home.php

--- dolla.php
+++ dolla.php
@@ -30,26 +30,10 @@
 <br><br>
 </div>
 
-<div id="footer">
-	<table>
-		<tr>
-			<td width = "100px"><a href="index.php">Home</a></td>
-			<td width = "100px"><a href="dolla.php">Games</a></td>
-			<td width = "100px"><a href="signIn.php">Log In</a></td>
-			<td width = "100px"><a href="register.php">Sign Up</a></td>
-		</tr>
+<?php
+	include_once('footer.php');
+?>
 		
-		<tr><td><br></td></tr>
-
-		<tr>
-			<td width = "100px"><a href="tictactoe.html">Tic Tac Toe</a></td>
-			<td width = "100px"><a href="memorycard.html">Memory Card</a></td>
-			<td width = "100px"><a href="blockbreaker.html">Brick Basher</a></td>
-			<td width = "100px"><a href="hangman.html">Hang Man</a></td>
-			<td width = "100px"><a href="navigator.html">Navigator</a></td>
-		</tr>
-	</table>
-</div>
 
 
 </body>
