bf758190a685a7356f273954dc28c71ce649db69
root <root@ip-172-31-28-37.us-west-2.compute.internal>
2016-11-11 21:38:17 +0000

    cleaned up login page

--- views/not_logged_in.php
+++ views/not_logged_in.php
@@ -15,16 +15,93 @@ if (isset($login)) {
 ?>
 
 <!-- login form box -->
+<style>
+ /* Bordered form */
+form {
+    border: 3px solid #f1f1f1;
+}
+
+/* Full-width inputs */
+input[type=text], input[type=password] {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    box-sizing: border-box;
+}
+
+/* Set a style for all buttons */
+button {
+    background-color: #4CAF50;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    cursor: pointer;
+    width: 100%;
+}
+
+/* Extra style for the cancel button (red) */
+.cancelbtn {
+    width: auto;
+    padding: 10px 18px;
+    background-color: #f44336;
+}
+
+/* Center the avatar image inside this container */
+.imgcontainer {
+    text-align: center;
+    margin: 24px 0 12px 0;
+}
+
+/* Avatar image */
+img.avatar {
+    width: 40%;
+    border-radius: 50%;
+}
+
+/* Add padding to containers */
+.container {
+    padding: 16px;
+}
+
+/* The "Forgot password" text */
+span.psw {
+    float: right;
+    padding-top: 16px;
+}
+
+/* Change styles for span and cancel button on extra small screens */
+@media screen and (max-width: 300px) {
+    span.psw {
+        display: block;
+        float: none;
+    }
+    .cancelbtn {
+        width: 100%;
+    }
+}
+</style>
 <form method="post" action="signIn.php" name="loginform">
+ <div class="imgcontainer">
+  <img src="hangman.png" alt="DollaTree" class"avater">
+ </div>
+ <div class="container">
+    <!--<label for="login_input_username">Username (or Email)</label>-->
+    <input id="login_input_username" class="login_input" type="text" name="user_name" placeholder="Username or Email" required />
 
-    <label for="login_input_username">Username (or Email)</label>
-    <input id="login_input_username" class="login_input" type="text" name="user_name" required />
+    <label for="login_input_password"> </label>
+    <input id="login_input_password" class="login_input" type="password" name="user_password" placeholder="Password" autocomplete="off" required />
 
-    <label for="login_input_password">Password</label>
-    <input id="login_input_password" class="login_input" type="password" name="user_password" autocomplete="off" required />
+    <button type="submit" name="login" value="Log in">Login</button>
+    <!--<input type="submit"  name="login" value="Log in" />-->
+ </div>
 
-    <input type="submit"  name="login" value="Log in" />
-
+ <div class="container" style="background-color:#f1f1f1">
+  <button type="button" class="cancelbtn">Cancel</button>
+  <span class="psw">Forgot <a href="#">password?</a></span>
+ </div>
 </form>
 
 <a href="register.php">Register new account</a>
