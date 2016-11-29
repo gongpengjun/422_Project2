1e04ad3d0f7bc628a19f8fdf0fd9e923c8c131ac
root <root@ip-172-31-28-37.us-west-2.compute.internal>
2016-11-29 01:31:41 +0000

    tried implementing memorycard game but George needs to do it

--- memorycard.php
+++ memorycard.php
@@ -3,7 +3,6 @@
 
 <title>Dolla Tree Memorycard</title>
 <link href="dolla.css" rel="stylesheet" type="text/css" />
-<script language = "JavaScript" src = "memorygame/js/memory.js"></script>
 </head>
 
 <body>
@@ -18,8 +17,89 @@
 </div>
 
 <div id="pageStuff">
-
 </div>
+
+<div id="my-memory-game"></div>
+
+<script src="js/classList.min.js"></script>
+<script src="memorygame/js/memory.js"></script>
+<script>
+    (function(){
+      var myMem = new Memory({
+        wrapperID : "my-memory-game",
+        cards : [
+          {
+            id : 1,
+            img: "memorygame/img/default/beavercartoon.png"
+          },
+          {
+            id : 2,
+            img: "memorygame/img/default/cartoonbeav.png"
+          },
+          {
+            id : 3,
+            img: "memorygame/img/default/cartoongeese.png"
+          },
+          {
+            id : 4,
+            img: "memorygame/img/default/cartoongoose.png"
+          },
+          {
+            id : 5,
+            img: "memorygame/img/default/donaldduck.png"
+          },
+          {
+            id : 6,
+            img: "memorygame/img/default/ducklings.png"
+          },
+          {
+            id : 7,
+            img: "memorygame/img/default/duck.png"
+          },
+          {
+            id : 8,
+            img: "memorygame/img/default/goose1.png"
+          },
+          {
+            id : 9,
+            img: "memorygame/img/default/goose.png"
+          },
+          {
+            id : 10,
+            img: "memorygame/img/default/logo-bw.png"
+          },
+          {
+            id : 11,
+            img: "memorygame/img/default/OSUbeav.png"
+          },
+          {
+            id : 12,
+            img: "memorygame/img/default/osu.png"
+          },
+          {
+            id : 13,
+            img: "memorygame/img/default/perry.png"
+          },
+          {
+            id : 14,
+            img: "memorygame/img/default/playtapus1.png"
+          },
+          {
+            id : 15,
+            img: "memorygame/img/default/quackyduck.png"
+          },
+          {
+            id : 16,
+            img: "memorygame/img/default/rubberduck.png"
+          }
+        ],
+        onGameStart : function() { return false; },
+        onGameEnd : function() { return false; }
+      });
+    })();
+
+</script>
+</div
 
 <table border="1" align="center">
 	<tr>
